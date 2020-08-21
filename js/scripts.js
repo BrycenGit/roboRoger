fee5332321fc212a7e578da596c72d6329bee92e
[brycen] <[brycenbartolome@gmail.com]>
2020-08-21 15:23:03 -0700

    add input to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
-let number = 4 ;
-let array = [];
-for (i=0; i<=number; i++) {
-  array.push('' + i);
-  console.log(array[i]);
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    let number = $("#number").val();
+    let array = [];
+    for (i=0; i<=number; i++) {
+      array.push(' ' + i);
+      console.log(array[i]);
 
 
-  
-}
-for(i = 0; i < array.length; i++)
-{
-    array[i] = array[i].replace('3', "Won't you be my neighbor?");
+      
+    }
+    for(i = 0; i < array.length; i++)
+    {
+        array[i] = array[i].replace(/3/g, "Won't you be my neighbor?");
 
-    array[i] = array[i].replace('1', "Beep!");
+        array[i] = array[i].replace(/1/g, "Beep!");
 
-    array[i] = array[i].replace('2', "Boop!");
-}
+        array[i] = array[i].replace(/2/g, "Boop!");
+    }
 
-
-console.log(array);
+    $("#show").text(array);
+    console.log(array);
+  });
+});

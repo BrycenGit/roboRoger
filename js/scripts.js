eaeafdde6fad74a8095dcf3d5027c21465cd547c
[brycen] <[brycenbartolome@gmail.com]>
2020-08-21 11:10:40 -0700

    add function to replace 3 with would you be my neighbor

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 let number = 4 ;
 let array = [];
 for (i=0; i<=number; i++) {
-  array.push(parseInt('' + i));
+  array.push('' + i);
   console.log(array[i]);
-  
+
 }
+for(i = 0; i < array.length; i++)
+{
+    array[i] = array[i].replace('3', "Won't you be my neighbor?");
+}
+
 console.log(array);
